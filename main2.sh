126dbdc071dde742a3cd61bfc59c0a619ca14987
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2022-05-26 17:08:05 -0400

    Delete main2.sh

--- main2.sh
+++
@@ -1,9 +0,0 @@
-export $(cat .env | xargs)
-{
-    g++ -std=c++17 src2/*.cpp -o bot -ldpp -g -O0
-} || {
-    sudo apt-get install -y libopus0 libsodium23
-    sudo dpkg -i /workspace/dpp-on-repl/libdpp-10.0.7-linux-x64.deb
-    g++ -std=c++17 src2/*.cpp -o bot -ldpp -g -O0
-}
-./bot
